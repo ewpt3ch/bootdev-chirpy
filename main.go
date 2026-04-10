5a02913a57df678a35b91f7cddfeaf6fdf1c169b
Eric Phillips <eric@ewpt3ch.com>
2026-04-10 11:01:15 -0600

    CH2L2: stateful handlers

--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const filepathRoot = "static"
 	const filepathApp = "/app/"
 	const port = "8080"
 
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle(filepathApp, http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle(filepathApp, http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("/reset", cfg.handlerReset)
 
 	httpServe := &http.Server{
 		Addr:    ":" + port,
@@ -26,4 +45,19 @@ func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func (c *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	hits := c.fileserverHits.Load()
+	respString := fmt.Sprintf("Hits: %v", hits)
+	w.Write([]byte(respString))
+}
+
+func (c *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	c.fileserverHits.Store(0)
+	w.Write([]byte("metrics reset to 0"))
 }
