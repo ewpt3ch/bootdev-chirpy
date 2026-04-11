03046e09fafa7cfc98706fba4e5fdb315be9706e
Eric Phillips <eric@ewpt3ch.com>
2026-04-11 17:18:02 -0600

    CH4L2: JSON

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -29,9 +30,10 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle(filepathApp, http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("GET /healthz", handlerReadiness)
-	mux.HandleFunc("GET /metrics", cfg.handlerMetrics)
-	mux.HandleFunc("POST /reset", cfg.handlerReset)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /admin/reset", cfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	httpServe := &http.Server{
 		Addr:    ":" + port,
@@ -48,10 +50,11 @@ func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 }
 
 func (c *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
 	hits := c.fileserverHits.Load()
-	respString := fmt.Sprintf("Hits: %v", hits)
+	htmlString := "<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>"
+	respString := fmt.Sprintf(htmlString, hits)
 	w.Write([]byte(respString))
 }
 
@@ -60,4 +63,49 @@ func (c *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(200)
 	c.fileserverHits.Store(0)
 	w.Write([]byte("metrics reset to 0"))
+}
+
+func handlerValidateChirp(w http.ResponseWriter, req *http.Request) {
+
+	type reqParameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	reqParams := reqParameters{}
+	err := decoder.Decode(&reqParams)
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong")
+		return
+	}
+
+	if len(reqParams.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	type respParams struct {
+		Valid bool `json:"valid"`
+	}
+
+	respondWithJSON(w, 200, respParams{Valid: true})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type returnVals struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, returnVals{Error: msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
 }
